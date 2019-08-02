a455920105e7037e85ab914d73f3984501f64f86
ahmad shah <ahmad.shahbcs@gmail.com>
2019-08-02 14:01:04 +0430

    the new class created

---
+++ DemoWebApp/src/main/java/com/habib/trains/TheNewClass.java
@@ -0,0 +1,10 @@
+package com.habib.trains;
+
+public class TheNewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hallo hallo the man he");
+	}
+
+}
